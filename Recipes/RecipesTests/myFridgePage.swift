00ffcc0a8e30c9f249dda172d82f2a96925cbf30
BedirT <bedir.tapkan.53@gmail.com>
2015-10-26 14:15:59 -0500

    updated design

---
+++ Recipes/RecipesTests/myFridgePage.swift
@@ -0,0 +1,21 @@
+//
+//  myFridgePage.swift
+//  Recipes
+//
+//  Created by Mustafa Bedir Tapkan on 10/26/15.
+//  Copyright © 2015 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class myFridgePage: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ recipeApplication/recipeApplication/checkBox.swift
@@ -0,0 +1,43 @@
+//
+//  checkBox.swift
+//
+//
+//  Created by Mustafa Bedir Tapkan on 10/26/15.
+//  Copyright © 2015 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class checkBox: UIButton {
+    
+    //images
+    let checkedImage = UIImage(named: "button_checkboxFilled")
+    let uncheckedImage = UIImage(named: "button_checkboxEmpty")
+    
+    // Bool Property
+    var isChecked : Bool = false {
+        didSet{
+            if isChecked == true{
+                self.setImage(checkedImage, forState: .Normal)
+            }else {
+                self.setImage(uncheckedImage, forState: .Normal)
+            }
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.addTarget(self, action: "buttonClicked:" , forControlEvents: UIControlEvents.TouchUpInside)
+        self.isChecked = false
+    }
+    
+    func buttonClicked (sender:UIButton) {
+        if (sender == self) {
+            if isChecked == true {
+                isChecked = false
+            }else {
+                isChecked = true
+            }
+        }
+    }
+}
+
