476782d286852c1356610f548686a708eb581522
skaraalioglu <seymanur.karaalioglu@gmail.com>
2015-10-26 10:15:57 -0500

    .

---
+++ Recipes/Recipes/recipeItems.swift
@@ -0,0 +1,83 @@
+//
+//  recipeItems.swift
+//  Recipes
+//
+//  Created by Seyma Nur Karaalioglu on 10/25/15.
+//  Copyright © 2015 Big Nerd Ranch. All rights reserved.
+//
+
+import Foundation
+
+
+struct KitchenLibrary {
+    
+    let library = [
+        [
+            "title": "Simple And Healty Salmon",
+            "directions": "1.Lay each salmon fillet onto a piece of aluminum foil skin-side down. Add salt and freshly ground black pepper. 2.Wrap each fillet tightly in the foil. Be sure to make an airtight seal. I fold the foil lengthwise and continue to fold along the seam until the fish is firmly wrapped. Fold/roll in each remaining end until the fish is fully enclosed. 3.Place the wrapped salmon fillets into a large saucepan and cover completely with cold water. Bring to the boil over a medium heat. Reduce heat and simmer gently for 3 minutes. 4.Remove the parcels from the saucepan. Carefully open each parcel and remove the skin from the fish. 5.Serve the salmon topped with your choice of toppings such as salsa, pesto, mango salsa, chunky tomato. Serve with a salad or steamed veges.",
+            "backgroundColor": ["red": 255, "green": 204, "blue": 51, "alpha": 1.0],
+            "items": ["Salmon", "Salt", "Black pepper"]
+        ],
+        [
+            "title": "Boiled Eggs",
+            "directions": "1.Place the eggs in a medium saucepan and cover with room temperature water. Bring the eggs to a boil, remove from the heat and let sit for 8 minutes. Shock the eggs in cold water to stop the cooking and shrink the egg from the shell. Peel.",
+            "backgroundColor": ["red": 255, "green": 102, "blue": 51, "alpha": 1.0],
+            "items": ["6 Eggs", "2 cups water", "1 teaspoon salt"]
+        ],
+        [
+            "title": "Perfect Basic White Rice",
+            "directions": "1.In a saucepan with a good fitting lid bring water, salt and butter if desired to a boil. 2.Add rice and stir. 3.Cover and reduce heat to medium low. You will know that your temperature is correct if a little steam is visible leaking from the lid. A lot of steam means your heat is too high. 4.Cook for 20 minutes. 5.DO NOT LIFT LID! 6.The steam that is trapped inside the pan is what allows the rice to cook properly. 7.Remove from heat and fluff with fork. 8.Serve",
+            "backgroundColor": ["red": 153, "green": 102, "blue": 204, "alpha": 1.0],
+            "items": ["1 cup white rice", "2 cups water", "1 teaspoon salt", "1 teaspoon butter(optional)"]
+        ],
+        [
+            "title": "Baba Ghanoush",
+            "directions": "1.Preheat barbecue grill on high heat. Using a sharp knife or skewer, pierce eggplant 8 times. Place onto grill. Cover with barbecue lid or a large roasting pan. Cook for 40 minutes, turning every 10 minutes, or until soft. Remove to a plate to cool. 2.Cut eggplant in half. Use a large metal spoon to scoop out flesh. Discard skin. Place flash into a food processor with onions, garlic and lemon juice. Process until well combined. Transfer to a bowl. Add parsley, and salt and pepper. Stir to combine. Cover. Refrigerate until required.",
+            "backgroundColor": ["red": 51, "green": 153, "blue": 204, "alpha": 1.0],
+            "items": ["1 large eggplant", "3 onions,finally chopped", "2 garlic, crashed", "2 tablespoons lemon juice", "1/3 cup parsley,finally chopped"]
+        ],
+        [
+            "title": "Really Fudgy Brownies",
+            "directions": "1.Melt butter and unsweetened chocolate together; allow cooling 2.Mix together eggs and sugar, make sure they are blended well; add cooled chocolate, vanilla, and salt 3.Add flour and mix just until blended.4.Bake in a greased 9x13 pan at 325° for 30-35 minutes.",
+            "backgroundColor": ["red": 51, "green": 204, "blue": 102, "alpha": 1.0],
+            "artists": ["1 cup butter", "6 ounces unsweetened chocolate", "4 eggs", "2cups sugar", "1 teaspoon vanilla", "1/2 teaspoon salt", "1/2 cup flour"]
+        ],
+        [
+            "title": "Simple Healty Summer Salad",
+            "directions": "1.Assemble all ingredients and toss gently to mix well. 2.May be served with salad dressing of choice and crackers or bread.",
+            "backgroundColor": ["red": 255, "green": 102, "blue": 153, "alpha": 1.0],
+            "items": ["1/2 head lettuce,washed, torn into bite sized pieces", "4 ounces spinach, fresh whole leaf", "1 cucumber,peeled, quarter, and sliced thin", "1 fresh tomato,washed and minced"]
+        ],
+        
+        
+        [
+            "title": "Fired Mashed Potatoes",
+            "directions": "1.Add the onion to the mashed potatoes. 2.Form into hamburger shape. 3.Dip patties in flour on both sides. 4.Fry in some hot grease until golden brown on each side.",
+            "backgroundColor": ["red": 255, "green": 102, "blue": 153, "alpha": 1.0],
+            "items": ["Mashed Potatoes (we used left over mash potatoes", "1 onion,diced"]
+          ],
+        
+        [
+            "title": "Gnocchi",
+            "directions": "1.Bring a large pot of salted water to a boil. Peel potatoes and add to pot. Cook until tender but still firm, about 15 minutes. Drain, cool and mash with a fork or potato masher 2.Combine 1 cup mashed potato, flour and egg in a large bowl. Knead until dough forms a ball. Shape small portions of the dough into long 'snakes'. On a floured surface, cut snakes into half-inch pieces 3.Bring a large pot of lightly salted water to a boil. Drop in gnocchi and cook for 3 to 5 minutes or until gnocchi have risen to the top; drain and serve. ",
+            "backgroundColor": ["red": 255, "green": 102, "blue": 153, "alpha": 1.0],
+            "items": ["2 potatoes", "1 egg", "2 cup all purpose flour"]
+        ],
+        
+        [
+            "title": "Chocolate Truffle Cake",
+            "directions": "1.Melt chocolate with butter. Cool. 2.Beat 2/3 Cup of sugar with egg yolks till thick and lemon coloured.3.Add Chocolate mixture and flour. blend well.4.Beat egg whites till soft peaks form. Add 2 Tbsp sugar and beat till stiff and shiny peaks form. 5.Fold Chocolate mixture into egg whites. Blend well.6.Pour into greased and floured 9 springform pan. 7.Bake at 375 for 35 - 40 minute.8.Cake will be moist in centre. Cool.9.Cake will fall in centre.10.Melt chocolate with butter and water. 11.Blend until smooth.12.Spread over cake.13.Garnish:.14.Melt chocolate. Put in Sandwich bag and cut a very small hole in 1 corner. Decorate cake with con centric circles. Starting at the centre draw a knife lightly (do not cut cake) through circles to outside edge. This will create a pattern similar to spider web.",
+            "backgroundColor": ["red": 255, "green": 102, "blue": 153, "alpha": 1.0],
+            "items": ["6(1 ounce) semi-sweet chocolate baking squares ", "3/4 cup butter", "2/3 cup granulated sugar", "4 eggs yolks", "1/3 cup all-purpose flour", "4 egg whites", "2 tablespoons granulated sugar"]
+        ],
+        
+        [
+            "title": "Pan Seared Steak",
+            "directions":"1.Remove steak(s) from fridge and start bringing them to room temperature. 2.After 30 to 45 minutes, place a 10 to 12-inch cast iron skillet in oven and heat oven to 500 degrees F. 3.When oven reaches temperature, remove the pan and place it on a burner over high heat.4.Coat steak(s) lightly with oil and season both sides with a generous pinch of salt. Grind on black pepper to taste.5.Immediately place steak(s) in the middle of the hot, dry pan. Cook 30 to 60 seconds without moving. Turn the steak(s) with tongs and cook another 30 to 60 seconds, then put the pan straight into the oven for 3 to 5 minutes. Flip steak(s) and cook for another 3 to 5 minutes. (The time in the oven depends on how rare you like your steaks. The time given is for medium, but depends on number of steaks, etc. as well.) 6.Remove the steak(s) from the pan, cover loosely with foil, and rest for 2 minutes. 7.Serve whole or slice thin and fan onto plate.",
+            "backgroundColor": ["red": 255, "green": 102, "blue": 153, "alpha": 1.0],
+            "items": ["Steaks 1/2 inch thick ", "Vegatable oil, to coat", "Salt & ground pepper"]
+        ]
+
+
+    ]
+}
