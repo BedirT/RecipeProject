86800b5873e411ad28f7e40335c0cf82d31d6f7c
BedirT <bedir.tapkan.53@gmail.com>
2015-11-10 18:07:58 -0600

    changedToTableView

--- recipeApplication/recipeApplication/ViewController.swift
+++ recipeApplication/recipeApplication/ViewController.swift
@@ -10,52 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    ///////////////////////////////////
-    //////////// MY LABELS ////////////
     
-    @IBOutlet weak var label1: UILabel!
-    @IBOutlet weak var label2: UILabel!
-    @IBOutlet weak var label3: UILabel!
-    @IBOutlet weak var label4: UILabel!
-    @IBOutlet weak var label5: UILabel!
-    @IBOutlet weak var label6: UILabel!
-    @IBOutlet weak var label7: UILabel!
-    @IBOutlet weak var label8: UILabel!
+    @IBOutlet weak var ingredientTable: UITableView!
     
-    func labelsAreHere () -> [Int] {
-        var label: Array = [label1,label2,label3,label4,label5,label6,label7,label8]
-        var myTag: Array = [1]
-        myTag.removeAll()
-        for var a = 0 ; a < label.count ; a++ {
-             myTag.append(label[a].tag)
-        }
-        return myTag
-    }
     
-    ///////////////////////////////////
-    
-    ///////////////////////////////////
-    //////////// CHECKBOXES ///////////
-    
-    @IBAction func box1(sender: checkBox) {
-    }
-    @IBAction func box2(sender: checkBox) {
-    }
-    @IBAction func box3(sender: checkBox) {
-    }
-    @IBAction func box4(sender: checkBox) {
-    }
-    @IBAction func box5(sender: checkBox) {
-    }
-    @IBAction func box6(sender: checkBox) {
-    }
-    @IBAction func box7(sender: checkBox) {
-    }
-    @IBAction func box8(sender: checkBox) {
-    }
-    
-    ///////////////////////////////////
-    ///////////////////////////////////
     
     ///////////////////////////////////
     /////////// SOCIAL LINKS //////////
@@ -80,11 +38,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     
     }
-<<<<<<< HEAD
-=======
-        
-
->>>>>>> 363056b777ee7733ba39afbb564c9fa604312168
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
